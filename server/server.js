3a575aaf6061b193e7696113e951acd3be28e75c
illrem <46941309+illrem@users.noreply.github.com>
2022-03-15 15:28:11 +0000

    gamecode debug

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ io.on('connection', (socket)=> {
         });
         socket.on("joinGame", function(data) {            
             //socket.emit("Print", "join request recieved: " + data)
-            
+            let roomName = data;
             rooms[socket.id] = roomName;
 
             socket.join(roomName);
