0c2c1ebe797a0a34cc6bbc23342e31dc1e917e33
illrem <46941309+illrem@users.noreply.github.com>
2022-03-15 00:57:13 +0000

    deploy

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const server = require('http').createServer()
 const io = require('socket.io')(server, {
     cors: {
-        origin: "http://localhost:8080",
+        origin: "https://tactictoe.co",
         methods: ["GET", "POST"]
     }
 });
