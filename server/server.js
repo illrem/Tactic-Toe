a84a652686d2af46d4bb6d8f3f284d7c5e11229c
illrem <46941309+illrem@users.noreply.github.com>
2022-03-16 15:56:43 +0000

    server bug fix

--- server/server.js
+++ server/server.js
@@ -59,7 +59,7 @@ io.on('connection', (socket)=> {
         socket.on("undoRequest", function(data) {
             io.to(rooms[socket.id]).emit("undoRequest");
         });
-        socket.on("undoRequest", function(data) {
+        socket.on("acceptUndoRequest", function(data) {
             io.to(rooms[socket.id]).emit("undo");
         })
 })
