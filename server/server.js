34cb572977c0ea98cf48247bf1eac89d06d7f231
illrem <46941309+illrem@users.noreply.github.com>
2022-03-17 03:10:38 +0000

    array store testing

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ io.on('connection', (socket)=> {
         //socket.broadcast.emit("play", {bigIndex:bigIndex, index:index})
         io.to(rooms[socket.id]).emit("play", {bigIndex:bigIndex, index:index});        
         //io.to(roomName).emit("Print", "Play recieved");
-        //socket.broadcast.emit("Print",moves[roomName][0])
+        socket.broadcast.emit("Print",moves[rooms[socket.id]][1])
         });
         socket.on("newGame", function(data) {
             
@@ -36,7 +36,7 @@ io.on('connection', (socket)=> {
             roomno++;
             let roomName = roomno.toString();
             rooms[socket.id] = roomName;
-            //moves[roomName][0] = "move 1";
+            moves[roomName] = [1, "move 1"];
             //socket.join(roomName);
 
             //await 
