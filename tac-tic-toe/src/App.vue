89df5058333b714591d62cb02c337b0429d1368d
illrem <46941309+illrem@users.noreply.github.com>
2022-03-17 02:43:18 +0000

    Update App.vue

--- tac-tic-toe/src/App.vue
+++ tac-tic-toe/src/App.vue
@@ -39,7 +39,6 @@
     <h1>TacTic Toe</h1>    
     <p>Turn Time: {{ countDown }} seconds remaining!</p>    
     <div class="game">
-      <h2 class="squareOverlay" id="winner" v-if="complete"> Winner is {{winner}} </h2>
       <div v-for="bigIndex in 9" v-bind:key="bigIndex" :id="'square_' + (bigIndex-1)" class="square" v-bind:class="{occupied:!allowed[bigIndex-1]}">    
         <div class="miniBoard">    
           <div @click="onlinePlay(bigIndex-1, index-1)" v-for="index in 9" v-bind:key="index"  :id="'square_' + (index-1)" class='miniSquare' v-bind:class="{occupied:occupied[bigIndex-1][index-1], lastMove:lastMove[bigIndex-1][index-1]}"  >{{board[bigIndex-1][index-1]}}</div>
